96873d694f518cbd61d454d4cf08ade647dff72a
lior2911 <aytegev1@gmail.com>
2022-06-11 23:05:41 +0300

    2COMMIT

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 function clickSubmit(){
-  if(checkFirstName()&&checkLastName()&&checkEmail()==true){
+  if(checkFirstName()&&checkLastName()&&checkEmail()&&checkBirth()&&checkPhone()==true){
     return true;
   }
   return false;
@@ -10,8 +10,8 @@ function checkFirstName(){
   if(fName_reg.test(firstName.value)){
     return true;
   }
-  fName_label.innerHTML +="<span> first latter <b>must</b> be uppercase </span>"
-  fName_label.style.color = "red"
+  fName_label.innerHTML +="<span> first latter <b>must</b> be uppercase </span>";
+  fName_label.style.color = "red";
   return false;
 }
 
@@ -20,17 +20,36 @@ function checkLastName(){
   if(lName_reg.test(lastName.value)){
     return true;
   }
-  lName_label.innerHTML +="<span> first latter <b>must</b> be uppercase </span>"
-  lName_label.style.color = "red"
+  lName_label.innerHTML +="<span> first latter <b>must</b> be uppercase </span>";
+  lName_label.style.color = "red";
   return false;
 
 }
 function checkEmail(){
-  var mail_reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  var mail_reg = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(mail_reg.test(mail.value)){
     return true;
   }
-  mail_label.innerHTML +="<span> first latter <b>must</b> be uppercase </span>"
-  mail.style.color = "red"
+  mail_label.innerHTML +="<span> first latter <b>must</b> be uppercase </span>";
+  mail.style.color = "red";
   return false;
+}
+function checkBirth(){
+  var nowDate = new Date().getFullYear();
+  var birthInput = parseInt(birthDate.value.substr(0,4))
+  if(nowDate-birthInput>18){
+    return true;
+  }
+  alert("you must be 18 or over");
+  return false;
+}
+function checkPhone(){
+  var phone_reg = /^[0-9]{10}$/;
+  if(phone_reg.test(phoneNumber.value)){
+    return true;
+  }
+  phone_label.innerHTML +="<span> phone number must containe 10 numbers </span>";
+  phone_label.style.color = "red";
+  return false;
+
 }
